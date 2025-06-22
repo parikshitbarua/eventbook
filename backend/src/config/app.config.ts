fc44bd3ed3ca8577c7b88e387ab051be8c31aff3
parikshitbarua <parikshitb04@gmail.com>
2025-06-22 21:22:18 +0530

    feat(app): added vercel config

--- backend/src/config/app.config.ts
+++ backend/src/config/app.config.ts
@@ -10,7 +10,8 @@ export const allowedOrigins = {
     "http://localhost:5173",
     "http://localhost:3000",
     "http://127.0.0.1:5173",
-    "http://127.0.0.1:3000"
+    "http://127.0.0.1:3000",
+    "https://eventbook-ashy.vercel.app/"
   ],
   QA: [
     // Add QA origins here
